d3ace9f6c05c2d53b84766e4ca6beb8b43c799e3
ChiragSuthar <sutharchirag128@gmail.com>
2022-02-11 20:35:09 +0530

    Go Back to Home ki jagah Profile

--- src/main/java/com/example/bazaar/BazaarApplication.java
+++ src/main/java/com/example/bazaar/BazaarApplication.java
@@ -56,20 +56,58 @@ public class BazaarApplication implements CommandLineRunner {
 //        shareDetail share2 = new shareDetail("comp3", 241D);
 //        shareRepo.save(share2);
 
-//        User user = new User("President", "Avnee", "Satija", "BnbPresident", "bullsandbears@gmail.com");
-//        user.setRole("ROLE_ADMIN");
+//        User user = new User("Riddhi1", "Riddhi", "Riddhi", "Riddhi", "bullsandbears@gmail.com");
+//        User user1 = new User("Riddhi2", "Riddhi", "Riddhi", "Riddhi", "bullsandbears@gmail.com");
+//        User user2 = new User("Nikhil", "Nikhil", "Nikhil", "Nikhil", "bullsandbears@gmail.com");
+//        User user3 = new User("Nikhil2", "Nikhil", "Nikhil", "Nikhil", "bullsandbears@gmail.com");
+//        User user4 = new User("Anika", "Nikhil", "Nikhil", "Anika", "bullsandbears@gmail.com");
+//        User user5 = new User("Hemang", "Nikhil", "Nikhil", "Hemang", "bullsandbears@gmail.com");
+//        User user6 = new User("Pragya", "Nikhil", "Nikhil", "Pragya", "bullsandbears@gmail.com");
+//        User user7 = new User("Vashisth", "Nikhil", "Nikhil", "Vashisth", "bullsandbears@gmail.com");
+//        User user8 = new User("Nikisha", "Nikhil", "Nikhil", "Nikisha", "bullsandbears@gmail.com");
+//        User user9 = new User("Avnee", "Nikhil", "Nikhil", "Avnee", "bullsandbears@gmail.com");
+//        User user10 = new User("Nikhil", "Nikhil", "Nikhil", "Nikhil", "bullsandbears@gmail.com");
+//        User user11 = new User("Ishaan2", "Nikhil", "Nikhil", "Ishaan", "bullsandbears@gmail.com");
+//        User user12 = new User("Riddhi2", "Nikhil", "Nikhil", "Riddhi", "bullsandbears@gmail.com");
+//        User user13 = new User("Vashishth2", "Nikhil", "Nikhil", "Vashishth", "bullsandbears@gmail.com");
+//        User user14 = new User("Bhavya2", "Nikhil", "Nikhil", "Bhavya", "bullsandbears@gmail.com");
+
+
+
+
+
 //        user.setPassword(encoder.encode(user.getPassword()));
-//        userRepo.save(user);
-
-//        User user1 = new User("GenSec", "Anika", "Dhadhich", "BnbGenSec", "bullsandbears@gmail.com");
-//        user1.setRole("ROLE_ADMIN");
 //        user1.setPassword(encoder.encode(user1.getPassword()));
-//        userRepo.save(user1);
-
-//        User user2 = new User("VicePresident", "Vashisth", "Doshi", "BnbVicePresident", "bullsandbears@gmail.com");
-//        user2.setRole("ROLE_ADMIN");
 //        user2.setPassword(encoder.encode(user2.getPassword()));
+//        user3.setPassword(encoder.encode(user3.getPassword()));
+//        user4.setPassword(encoder.encode(user4.getPassword()));
+//        user5.setPassword(encoder.encode(user5.getPassword()));
+//        user6.setPassword(encoder.encode(user6.getPassword()));
+//        user7.setPassword(encoder.encode(user7.getPassword()));
+//        user8.setPassword(encoder.encode(user8.getPassword()));
+//        user9.setPassword(encoder.encode(user9.getPassword()));
+//        user10.setPassword(encoder.encode(user10.getPassword()));
+//        user11.setPassword(encoder.encode(user11.getPassword()));
+//        user12.setPassword(encoder.encode(user12.getPassword()));
+//        user13.setPassword(encoder.encode(user13.getPassword()));
+//        user14.setPassword(encoder.encode(user14.getPassword()));
+
+//        userRepo.save(user);
+//        userRepo.save(user1);
 //        userRepo.save(user2);
+//        userRepo.save(user3);
+//        userRepo.save(user4);
+//        userRepo.save(user5);
+//        userRepo.save(user6);
+//        userRepo.save(user7);
+//        userRepo.save(user8);
+//        userRepo.save(user9);
+//        userRepo.save(user10);
+//        userRepo.save(user11);
+//        userRepo.save(user12);
+//        userRepo.save(user13);
+//        userRepo.save(user14);
+
 
 
 //        shareDetail share = new shareDetail("ICICI Bank", 2D);
